c1aec2091b9011f847883fa83c7d7b8d24528f09
RPOOVENDRAPANDI <rpoovendrapandi@outlook.com>
2023-07-09 21:42:36 +0530

    Commented django-extensions in installed aps due to pipeline error

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
+    # TODO: Getting error when enabled in pipeline. Need to fix later
+    # 'django_extensions',
     'core',
     'rest_framework',
     'rest_framework.authtoken',
