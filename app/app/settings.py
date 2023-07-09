dcb0feddc81a6d8f28e3f1f425bb3a7372c7c4c4
RPOOVENDRAPANDI <rpoovendrapandi@outlook.com>
2023-07-09 21:55:17 +0530

    Added django extensions in requirements

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # TODO: Getting error when enabled in pipeline. Need to fix later
-    # 'django_extensions',
+    'django_extensions',
     'core',
     'rest_framework',
     'rest_framework.authtoken',
