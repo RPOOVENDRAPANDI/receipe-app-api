4c880e2a0d4844e75e8c33ad5029e25e6101d578
RPOOVENDRAPANDI <rpoovendrapandi@outlook.com>
2023-07-09 21:18:29 +0530

    Rerun push for sjango_extensions

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
+    'django_extensions', # Throwing an error in pipeline
     'core',
     'rest_framework',
     'rest_framework.authtoken',
